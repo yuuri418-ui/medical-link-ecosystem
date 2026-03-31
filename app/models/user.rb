5b836f8e47f746071d0de5066bfdee33cdd40d41
doremi <yuuri.418@gmail.com>
2026-03-31 17:07:38 +0900

    性別の定義（数値と意味を紐付ける）

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # 性別の定義（数値と意味を紐付ける）
+  enum gender: { unselected: 0, male: 1, female: 2, other: 3 }
 end
+
