5fa6d598a33289a92598aa765e5de3f6f3a0c396
doremi <yuuri.418@gmail.com>
2026-04-01 20:36:30 +0900

    モデルのアソシエーションを追記

--- app/models/daily_log.rb
+++ app/models/daily_log.rb
@@ -1,5 +1,6 @@
 class DailyLog < ApplicationRecord
   belongs_to :user
+  has_many :temperature_logs, dependent: :destroy
 
   validates :date, presence: true, uniqueness: { scope: :user_id }
 end
--- app/models/temperature_log.rb
+++ app/models/temperature_log.rb
@@ -1,2 +1,5 @@
 class TemperatureLog < ApplicationRecord
+  belongs_to :daily_log
+
+  validates :value, presence: true, inclusion: { in: 30.0..45.0 }
 end
