0d1199b6dda773feb5eac4252bee1e05f15059f9
doremi <yuuri.418@gmail.com>
2026-04-01 14:43:23 +0900

    モデルの作成

---
+++ app/models/daily_log.rb
@@ -0,0 +1,2 @@
+class DailyLog < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_03_30_074633) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_01_053032) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "daily_logs", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.date "date", null: false
+    t.integer "stiffness_duration", default: 0
+    t.integer "pain_vas", default: 0
+    t.integer "fatigue_vas", default: 0
+    t.integer "condition", default: 3, null: false
+    t.text "memo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "date"], name: "index_daily_logs_on_user_id_and_date", unique: true
+    t.index ["user_id"], name: "index_daily_logs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,4 +49,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_03_30_074633) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "daily_logs", "users"
 end
