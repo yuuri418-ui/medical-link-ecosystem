ba70eac2105b7ccbf84541a506015584e509bec3
doremi <yuuri.418@gmail.com>
2026-04-01 20:33:40 +0900

    マイグレート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_04_01_053032) do
+ActiveRecord::Schema[7.1].define(version: 2026_04_01_112814) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,15 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_01_053032) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "date"], name: "index_daily_logs_on_user_id_and_date", unique: true
     t.index ["user_id"], name: "index_daily_logs_on_user_id"
+  end
+
+  create_table "temperature_logs", force: :cascade do |t|
+    t.bigint "daily_log_id", null: false
+    t.datetime "measured_at"
+    t.float "value", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["daily_log_id"], name: "index_temperature_logs_on_daily_log_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,4 +59,5 @@ ActiveRecord::Schema[7.1].define(version: 2026_04_01_053032) do
   end
 
   add_foreign_key "daily_logs", "users"
+  add_foreign_key "temperature_logs", "daily_logs"
 end
