ff4bfcdd7d67fba8ed3b94a24bf020b8d447bbd0
doremi <yuuri.418@gmail.com>
2026-04-02 10:39:55 +0900

    DailyLog モデルに子要素の保存を許可

--- app/models/daily_log.rb
+++ app/models/daily_log.rb
@@ -3,4 +3,7 @@ class DailyLog < ApplicationRecord
   has_many :temperature_logs, dependent: :destroy
 
   validates :date, presence: true, uniqueness: { scope: :user_id }
+
+  # 子要素の保存を許可し、中身が空なら無視する
+  accepts_nested_attributes_for :temperature_logs, allow_destroy: true, reject_if: :all_blank
 end
