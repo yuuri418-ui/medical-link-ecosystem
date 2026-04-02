e9078e4ba9d57a6f65d9c39025a108c75e36a0ff
doremi <yuuri.418@gmail.com>
2026-04-02 14:31:27 +0900

    new アクションを追記

--- app/controllers/visit_logs_controller.rb
+++ app/controllers/visit_logs_controller.rb
@@ -3,6 +3,10 @@ class VisitLogsController < ApplicationController
   end
 
   def new
+    @visit_log = current_user.visit_logs.build
+    # 最初から3つずつ入力欄を表示させる
+    3.times { @visit_log.blood_test_results.build }
+    3.times { @visit_log.prescribed_medicines.build }
   end
 
   def show
