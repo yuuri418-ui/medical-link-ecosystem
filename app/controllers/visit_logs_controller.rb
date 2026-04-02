870e097b037cbb6d0f9ee72d5739ef941bcf3fd6
doremi <yuuri.418@gmail.com>
2026-04-02 14:29:52 +0900

    ストロングパラメータを追記

--- app/controllers/visit_logs_controller.rb
+++ app/controllers/visit_logs_controller.rb
@@ -10,4 +10,14 @@ class VisitLogsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def visit_log_params
+    params.require(:visit_log).permit(
+      :visited_on, :hospital_name, :department, :doctor_name, :memo,
+      blood_test_results_attributes: [:id, :item_name, :value, :unit, :_destroy],
+      prescribed_medicines_attributes: [:id, :name, :dosage, :_destroy]
+    )
+  end
 end
