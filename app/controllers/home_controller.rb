d05761c2b35a781a4eb6dc10edc4d734904ad99b
doremi <yuuri.418@gmail.com>
2026-04-05 00:03:08 +0900

    修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,12 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
-  
+
   def index
+    if current_user
+      # カレンダー表示用に今月のログを取得
+      @daily_logs = current_user.daily_logs.includes(:temperature_logs)
+    else
+      @daily_logs = []
+    end
   end
 end
