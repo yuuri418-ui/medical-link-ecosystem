ad97370cf56dda5e5b9c9f4ec233c8751cbc0c00
doremi <yuuri.418@gmail.com>
2026-04-05 11:19:45 +0900

    修正

--- db/migrate/20260404023621_add_patient_id_to_users.rb
+++ db/migrate/20260404023621_add_patient_id_to_users.rb
@@ -1,5 +1,5 @@
-class AddPatientIdToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :patient_id, :string
-  end
-end
+#class AddPatientIdToUsers < ActiveRecord::Migration[7.1]
+  #def change
+    #add_column :users, :patient_id, :string
+  #end
+#end
